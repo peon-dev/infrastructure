9e7b91a523ce0bcf75378e258d102538d7ef6148
Jan Mikeš <j.mikes@me.com>
2023-07-16 21:53:09 +0200

    Deploy script - pull recipes + php specific image name

--- peon/deploy.sh
+++ peon/deploy.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-docker compose pull dashboard
+docker pull ghcr.io/peon-dev/php:main
+docker pull ghcr.io/peon-dev/php-recipes:main
 
 LOGS_START=$(expr $(date +%s))
 
 # Do not restart other services (Postgres, Mercure)
 # It they were not running, they will start automatically thanks to `depends_on`
 docker compose up --detach dashboard worker scheduler
-
 docker compose logs --since $LOGS_START dashboard worker scheduler
