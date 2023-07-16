59d9085abb7fdbbb1f0decf573b77f4691cb83c3
Jan Mikeš <j.mikes@me.com>
2023-07-16 10:42:49 +0200

    Print logs for deployment

--- peon/deploy.sh
+++ peon/deploy.sh
@@ -4,6 +4,10 @@ set -e
 
 docker compose pull dashboard
 
+LOGS_START=$(expr $(date +%s))
+
 # Do not restart other services (Postgres, Mercure)
 # It they were not running, they will start automatically thanks to `depends_on`
-docker-compose up dashboard worker scheduler -d
+docker compose up --detach dashboard worker scheduler
+
+docker compose logs --since $LOGS_START dashboard worker scheduler
